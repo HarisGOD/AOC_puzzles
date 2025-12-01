8bc489ea6cd98fbf3815751dedd3286351d6bfbe
thegod <kamil29june@yandex.ru>
2025-12-01 15:54:33 +0300

    init and solve day 1 p1 p2

--- src/main/kotlin/su/kamil/dev/tasks/t2_secret_entrance/SecondTaskSolution.kt
+++ src/main/kotlin/su/kamil/dev/tasks/t2_secret_entrance/SecondTaskSolution.kt
@@ -1,2 +1,44 @@
 package su.kamil.dev.tasks.t2_secret_entrance
 
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileReader
+
+// 1. "Due to newer security protocols, please use password method 0x434C49434B until further notice."
+//      You remember from the training seminar that "method 0x434C49434B"
+//      means you're actually supposed to count the number of times any click causes
+//      the dial to point at 0, regardless of whether it happens during a rotation or at the end of one.
+
+
+fun main() {
+    val pathToFile = "src/main/kotlin/su/kamil/dev/tasks/t1_secret_entrance/input"
+    val file = File(pathToFile)
+    val fileInputStream = FileInputStream(file)
+    val reader = FileReader(file)
+    val lines = reader.readLines()
+
+
+    // dial is numbers 0..99
+    val res = countClicks(lines)
+    println(res)
+}
+
+fun countClicks(lines: List<String>): Int {
+    var dial = 50
+    var zeros = 0
+
+
+    lines.forEach { line ->
+        val direction = if (line[0] == 'R') 1 else -1
+        val distance = line.removeRange(0, 1).toInt()
+        val curDial = dial
+        for(i in 0..<distance){
+            dial = (dial+direction).mod(100)
+            if (dial==0) zeros++
+        }
+    }
+
+    return(zeros)
+}
+
+
--- src/test/kotlin/su/kamil/dev/t2_secret_entrance/SecondTaskSolutionTest.kt
+++ src/test/kotlin/su/kamil/dev/t2_secret_entrance/SecondTaskSolutionTest.kt
@@ -1,44 +1,31 @@
 package su.kamil.dev.su.kamil.dev.t2_secret_entrance
 
+import org.junit.jupiter.api.Test
+import su.kamil.dev.tasks.t2_secret_entrance.countClicks
 import java.io.File
 import java.io.FileInputStream
 import java.io.FileReader
+import kotlin.test.assertEquals
 
 // 1. "Due to newer security protocols, please use password method 0x434C49434B until further notice."
 //      You remember from the training seminar that "method 0x434C49434B"
 //      means you're actually supposed to count the number of times any click causes
 //      the dial to point at 0, regardless of whether it happens during a rotation or at the end of one.
 
+class SecondTaskSolutionTest {
 
-fun main() {
-    val pathToFile = "src/main/kotlin/su/kamil/dev/tasks/t1_secret_entrance/input"
-    val file = File(pathToFile)
-    val fileInputStream = FileInputStream(file)
-    val reader = FileReader(file)
-    val lines = reader.readLines()
-
-
-    // dial is numbers 0..99
-    val res = countClicks(lines)
-    println(res)
-}
-
-fun countClicks(lines: List<String>): Int {
-    var dial = 50
-    var zeros = 0
+    @Test
+    fun testClicksCounter() {
+        val lines = listOf(
+            "R50",
+            "L111",
+            "R99"
+        )
 
 
-    lines.forEach { line ->
-        val direction = if (line[0] == 'R') 1 else -1
-        val distance = line.removeRange(0, 1).toInt()
-        val curDial = dial
-        for(i in 0..<distance){
-            dial = (dial+direction).mod(100)
-            if (dial==0) zeros++
-        }
+        val res = countClicks(lines)
+        assertEquals(3,res)
     }
 
-    return(zeros)
+
 }
-
-
